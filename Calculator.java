09b711e017d147dbb8bd3d783d635491009be709
Manoj Dongare <manojdon777@gmail.comfdsfsdf>
2023-04-28 14:34:20 +0530

    Lab session DOcker n Git asignment

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+class Calculator{
+	public static void main(String[] args){
+		int a = 10;
+		int b = 15;
+		System.out.println("Addition of " + a + " and " + b + " is         = " + (a + b));
+		System.out.println("Substraction of " + a + " and " + b + " is     = " + (a - b));
+		System.out.println("Multiplication of " + a + " and " + b + " is   = " + (a * b));
+		System.out.println("Division of " + a + " and " + b + " is         = " + (a / b));
+
+	}
+}
